03cb0bb7336d976127947a08b3249aa99ebe5335
adaiacamacho <wxlf.idk@gmail.com>
2025-08-15 12:30:38 +0200

    quee

--- index.js
+++ index.js
@@ -24,7 +24,26 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
-
+app.get("/api/:date?", function (req, res) {
+  let fecha;
+  let fe2;
+  if(req.params.date==null){
+    fecha=new Date();
+    fe2=Math.floor(fecha.getTime() / 1000);
+  }else if(req.params.date.includes("-")){
+    fecha= new Date(req.params.date);
+    fe2=Math.floor(fecha.getTime() / 1000);
+  }else{
+    fe2=parseInt(req.params.date);
+    fecha=new Date(req.params.date*1000);
+  }  
+  let correcto=!isNaN(new Date(fecha));
+  if(correcto){
+    res.json({unix: fe2, utc:fecha.toUTCString()});
+  }else{
+    res.json({error:"Invalid Date"})
+  } 
+});
 
 // Listen on port set in environment variable or default to 3000
 var listener = app.listen(process.env.PORT || 3000, function () {
